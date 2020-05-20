ee224c6a9201e64b823ad23103deb91d70d6f343
heeseonim <hisundl@naver.com>
2020-05-20 21:00:34 +0900

    [20200520 임희선] 로그인 & 회원가입 완료

--- frontend/mycareer/src/App.tsx
+++ frontend/mycareer/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Template1 from "./Components/Template1/Template1";
+import Login from "./Components/User/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/Template1" exact component={Template1} />
+          <Route path="/Login" exact component={Login} />
         </Switch>
       </BrowserRouter>
     </div>
