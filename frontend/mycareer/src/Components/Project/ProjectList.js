e2ea8571316a8636af982c8c4771cb7c41089404
heeseonim <hisundl@naver.com>
2020-05-22 12:28:15 +0900

    [20200522 임희선] 프로젝트 리스트 완료

--- frontend/mycareer/src/Components/Project/ProjectList.js
+++ frontend/mycareer/src/Components/Project/ProjectList.js
@@ -1,59 +1,124 @@
 import React from "react";
-import Board from "@lourenci/react-kanban";
 import "./ProjectList.scss";
+import { Row, Col } from "antd";
+import { Upload, Modal, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
-const board = {
-  columns: [
-    {
-      id: 1,
-      title: "My Career",
-      cards: [
-        {
-          id: 1,
-          title: "ㅎㅎㅎ",
-          description: "빅데이터 기반 장소 추천 서비스"
-        },
-        {
-          id: 2,
-          title: "Card title 2",
-          description: "Card content"
-        },
-        {
-          id: 3,
-          title: "Card title 3",
-          description: "Card content"
-        }
-      ]
-    }
-  ]
-};
-
-function UncontrolledBoard() {
-  return (
-    <Board
-      allowRemoveLane
-      allowRenameColumn
-      allowRemoveCard
-      onLaneRemove={console.log}
-      onCardRemove={console.log}
-      onLaneRename={console.log}
-      initialBoard={board}
-      allowAddCard={{ on: "top" }}
-      onNewCardConfirm={draftCard => ({
-        id: new Date().getTime(),
-        ...draftCard
-      })}
-      onCardNew={console.log}
-    />
-  );
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
 }
 
-function ProjectList() {
-  return (
-    <>
-      <UncontrolledBoard />
-    </>
-  );
+class ProjectList extends React.Component {
+  state = {
+    previewVisible: false,
+    previewImage: "",
+    previewTitle: "",
+    fileList: [
+      {
+        uid: "-1",
+        name: "image.png",
+        status: "done",
+        url: "https://www.simplesite.com/Images/FrontPage2017/Themes/4.png",
+      },
+      {
+        uid: "-2",
+        name: "image.png",
+        status: "done",
+        url: "https://www.thebestdesigns.com/design-images/artoftea.com-1.jpg",
+      },
+      {
+        uid: "-3",
+        name: "image.png",
+        status: "done",
+        url: "https://cdn.imweb.me/thumbnail/20200131/b13f5004d887a.png",
+      },
+      {
+        uid: "-4",
+        name: "image.png",
+        status: "done",
+        url:
+          "https://www.macobserver.com/imgs/tips/apple_site_no_shopping.jpg",
+      },
+    ],
+  };
+
+  handleCancel = () => this.setState({ previewVisible: false });
+
+  handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+
+    this.setState({
+      previewImage: file.url || file.preview,
+      previewVisible: true,
+      previewTitle:
+        file.name || file.url.substring(file.url.lastIndexOf("/") + 1),
+    });
+  };
+
+  handleChange = ({ fileList }) => this.setState({ fileList });
+
+  render() {
+    const { previewVisible, previewImage, fileList, previewTitle } = this.state;
+    const uploadButton = (
+      <div>
+        <PlusOutlined />
+        <div className="ant-upload-text">새 프로젝트 추가</div>
+      </div>
+    );
+    return (
+      <>
+        <Row className="ProjectRow">
+          <Col span={12} offset={6}>
+            <div className="MainDiv">
+              <h2>My Career</h2>
+              <br />
+              <Row>
+              <Col span={12}>
+              <Button>인적사항 수정</Button>
+              &nbsp;&nbsp;
+              <Button>회원정보 수정</Button>
+              </Col>
+              <Col span={6}></Col>
+              <Col span={6}>
+              <Button type="primary">완성 포트폴리오 보기</Button>
+              </Col>
+              </Row>
+              <div className="clearfix">
+                <Upload
+                  action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                  listType="picture-card"
+                  fileList={fileList}
+                  onPreview={this.handlePreview}
+                  onChange={this.handleChange}
+                >
+                  {fileList.length >= 8 ? null : uploadButton}
+                </Upload>
+                <Modal
+                  visible={previewVisible}
+                  title={previewTitle}
+                  footer={null}
+                  onCancel={this.handleCancel}
+                >
+                  <img
+                    alt="example"
+                    style={{ width: "100%" }}
+                    src={previewImage}
+                  />
+                </Modal>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </>
+    );
+  }
 }
 
 export default ProjectList;
