6ded41cc8a279b637f3db3e0e86da1e10e6474a4
surealyeun <monkyw@naver.com>
2020-05-22 15:23:26 +0900

    [20200522 한수연] merge 전 커밋

---
+++ frontend/mycareer/src/Components/template3/Template3.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Template3 extends React.Component {
+    render() {
+        return (
+            <div className="template3">
+                
+            </div>
+        );
+    }
+}
+
+export default Template3;
