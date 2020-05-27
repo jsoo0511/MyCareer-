628d6bf593ac094737506555c76efd5ef9e8a670
이정수 <jsoo94@naver.com>
2020-05-28 01:15:06 +0900

    [20200528 이정수] tech rest 일부 구현

--- backend/mycareer/src/main/java/com/mycareer/model/service/ProjectServiceImpl.java
+++ backend/mycareer/src/main/java/com/mycareer/model/service/ProjectServiceImpl.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mycareer.model.dto.project.Api;
+import com.mycareer.model.dto.project.Tech;
 import com.mycareer.model.repo.ApiRepository;
+import com.mycareer.model.repo.TechRepository;
 
 @Service
 public class ProjectServiceImpl implements ProjectService{
 
 	@Autowired
 	ApiRepository ar;
+	
+	@Autowired
+	TechRepository tr;
 	
 	/**Api관련 메서드*/
 	//apiNo를 통해 api하나에 대한 정보를 가져오기
@@ -54,6 +59,46 @@ public class ProjectServiceImpl implements ProjectService{
 	public int deleteByApiNo(int apiNo) {
 		try {
 			int result=ar.deleteByApiNo(apiNo);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	@Override
+	public Tech findByTechNo(int techNo) {
+		try {
+			Tech result=tr.findByTechNo(techNo);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	@Override
+	public List<Tech> findBytProjectProjectNo(int projectNo) {
+		try {
+			List<Tech> result=tr.findBytProjectProjectNo(projectNo);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	@Override
+	public Tech saveTech(Tech tech) {
+		try {
+			Tech result=tr.save(tech);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	@Override
+	public int deleteByTechNo(int techNo) {
+		try {
+			int result=tr.deleteByTechNo(techNo);
 			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
