45521f452cd28f49e60d5c66ac6fc20d9a220290
surealyeun <monkyw@naver.com>
2020-05-29 09:26:12 +0900

    [20200529 한수연] template3

--- frontend/mycareer/src/App.tsx
+++ frontend/mycareer/src/App.tsx
@@ -8,6 +8,7 @@ import UserInfo from "./Components/User/UserInfo";
 import TemplateList from "./Components/templateList/TemplateList";
 import PersonalInfo from "./Components/personalInfo/PersonalInfo";
 import Portfolio from "./Components/portfolio/Portfolio";
+import Tem3 from "./Components/template3/Template3";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/tp-list" component={TemplateList} />
           <Route path="/personal" component={PersonalInfo} />
           <Route path="/portfolio" component={Portfolio} /> {/* 뒤에 user id를 붙이는 형식으로 수정 */}
+          <Route path="/t3" component={Tem3} />
         </Switch>
       </BrowserRouter>
     </div>
